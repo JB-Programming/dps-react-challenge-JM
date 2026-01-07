e7fe0d66b04b6224a1ab5fe38f59a5d9de4b0551
JB-Programming <jb.programming.21@gmail.com>
2026-01-07 12:46:17 +0100

    Added fetch for City Name and Postal Code on Blur

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import dpsLogo from './assets/DPS.svg';
 import './App.css';
 
 //Test
+const fetchCityName = (postal_code: number) => {
+	fetch(
+		`https://openplzapi.org/de/Localities?postalCode=${postal_code}`,
+	)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+}
 
+const fetchPostalCode = (city_name: string) => {
+	fetch(
+		`https://openplzapi.org/de/Localities?name=${city_name}`,
+	)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+}
 
 function App() {
 	return (
@@ -14,8 +28,12 @@ function App() {
 			</div>
 			<div className="home-card">
 				<p>Your solution goes here 😊</p>
-				<input type="text" name="city_name" id="city_name" />
-				<input type="text" name="single_postal_code" id="single_postal_code" />
+				<input type="text" name="city_name" id="city_name" 
+				onBlur={(e) => fetchPostalCode(e.target.value)}
+				/>
+				<input type="text" name="single_postal_code" id="single_postal_code" 
+				onBlur={(e) => fetchCityName(parseInt(e.target.value))}
+				/>
 				<select name="multi_postal_code" id="multi_postal_code">
 					<option value="postal_code_1">Postal Code 1</option>
 				</select>
